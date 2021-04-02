8554ca387888c61f51c919aab764168f64777ab7
Code Wolf <brgerner@gmail.com>
2021-04-02 14:08:12 -0700

    scrapped CharCode method - used arrays instead

--- Develop/script.js
+++ Develop/script.js
@@ -1,104 +1,57 @@
-// const passwordElement = document.getElementById('password');
-// const generateElement = document.getElementById('generate');
+let generateBtn = document.querySelector("#generate");
+let passwordText = document.querySelector("#password");
 
-const rando = {
-  lower: randomLowerCase(),
-  upper: randomUpperCase(),
-  number: randomNumber(),
-  symbol: randomSymbol(),
-}
-
-
-function randomLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function randomUpperCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function randomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function randomSymbol() {
-  const symbols = '`~!@#$%^&*()-_=+[{]}\|;:",<.>/?';
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let randolower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let randoupper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let randonumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let randosymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "}", "]", ";", ":"]
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
-function generatePassword () {
+function generatePassword() {
 
-  const charOptionsArray = [];
+  const charTypesArray = [];
 
-  var pwLength = parseInt(prompt('How long would you like your password to be? (enter character count 8-128)'));
-    if (pwLength < 8 || pwLength > 128 || typeof(pwLength) != "number"|| pwLength === null) {
-      alert("You've chosen an invalid response. Please try again!");
-      generatePassword();
-    }
-  var pwLowerCase = confirm('Would you like to include lower case characters?');
+  let pwLength = parseInt(prompt('How long would you like your password to be? (enter character count 8-128)'));
+  if (pwLength < 8 || pwLength > 128 || isNaN(pwLength) || pwLength === null) {
+    alert("You've chosen an invalid response. Please try again!");
+    generatePassword();
+  }
+  else {
+    let pwLowerCase = confirm('Would you like to include lower case characters?');
     if (pwLowerCase === true) {
-      charOptionsArray.push(rando.lower)
+      charTypesArray.push(randolower)
     }
-  var pwUpperCase = confirm('Would you like to include upper case characters?');
-  if (pwUpperCase === true) {
-    charOptionsArray.push(rando.upper)
+    let pwUpperCase = confirm('Would you like to include upper case characters?');
+    if (pwUpperCase === true) {
+      charTypesArray.push(randoupper)
+    }
+    let pwNumbers = confirm('Would you like to include numbers?');
+    if (pwNumbers === true) {
+      charTypesArray.push(randonumber)
+    }
+    let pwSpecial = confirm('Would you like to include special characters?');
+    if (pwSpecial === true) {
+      charTypesArray.push(randosymbol)
+    }
+
+    if (pwLowerCase === false && pwUpperCase === false && pwNumbers === false && pwSpecial === false) {
+      alert("You must select at least one character type. Please refresh the page and try again!");
+    }
+
+    let generatedPassword = "";
+
+    while (generatedPassword.length < pwLength) {
+      for (let i = 0; i < charTypesArray.length; i++) {
+        if (generatedPassword.length < pwLength) {
+          let rando = Math.floor(Math.random() * charTypesArray[i].length)
+          generatedPassword += charTypesArray[i][rando]
+        }
+      }
+    }
+
+    passwordText.value = generatedPassword;
+
   }
-  var pwNumbers = confirm('Would you like to include numbers?');
-  if (pwNumbers === true) {
-    charOptionsArray.push(rando.number)
-  }
-  var pwSpecial = confirm('Would you like to include special characters?');
-  if (pwSpecial === true) {
-    charOptionsArray.push(rando.symbol)
-  }
-
-  let writtenPassword = '';
-  
-  const typesCount = rando.lower + rando.upper + rando.number + rando.symbol;
-
-  if(typesCount === 0) {
-    return '';
-  }
-
-  // length is character count from prompt
-  for (let i = 0; i < pwLength; i++) {
-
-    const typesArray = [rando.lower, rando.upper, rando.number, rando.symbol].filter
-      (item => Object.values(item)[0]);
-
-    typesArray.forEach(type => {
-      const randoName = Object.keys(type)[0];
-
-      var randomCharacter = typesArray[(Math.floor(Math.random() * typesArray.length))];
-
-      writtenPassword += randomCharacter;
-
-      console.log(writtenPassword);
-
-    });
-    
-  }
-  return writtenPassword
-}
-
-// Write password to the #password input
-function writePassword() {
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-  console.log(password);
-
-  return password;
-
 }
